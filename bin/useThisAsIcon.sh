e6cef0b1b72dc603c26ae51617f4a5d2cdd4284d
RaitaroHikami <19552720+RaitaroH@users.noreply.github.com>
2018-08-10 18:53:50 +0300

    Add files via upload

--- bin/useThisAsIcon.sh
+++ bin/useThisAsIcon.sh
@@ -5,4 +5,5 @@ file=$1
 directory=$(dirname "${file}")
 
 kioclient5 copy "thumbnail://$file" "$directory/.cover.png"
+# ln -fs "$file" "$directory/.cover"
 kwriteconfig5 --file "$directory/.directory" --group 'Desktop Entry' --key Icon ./.cover
