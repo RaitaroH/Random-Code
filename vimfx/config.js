06e4052b37382dbb81e6ecb7adf351f7ad4c6419
RaitaroHikami <19552720+RaitaroH@users.noreply.github.com>
2018-08-16 18:42:34 +0300

    Update config.js

--- vimfx/config.js
+++ vimfx/config.js
@@ -45,7 +45,6 @@ vimfx.set('custom.mode.normal.zoom_out', '@')
 //
 
 // Close tabs to the left
-
 let {commands} = vimfx.modes.normal
 vimfx.addCommand({
   name: 'tab_close_to_start',
@@ -61,7 +60,6 @@ vimfx.set('custom.mode.normal.tab_close_to_start', 'gx0')
 //
 
 // Close window
-
 vimfx.addCommand({
   name: 'window_close',
   description: 'Close window',
@@ -96,7 +94,6 @@ vimfx.addCommand({
 });
 vimfx.set('custom.mode.normal.restart', 'ZR')
 //
-
 
 //Search for the selected text
 vimfx.addCommand({
@@ -156,72 +153,12 @@ vimfx.addCommand({
 vimfx.set('custom.mode.normal.addons', '<c-e>')
 //
 
-// Play a link with an external media player
-const mpv_path = '/usr/bin/mpv'
-const mpv_options = '--video-unscaled=yes'
-
-vimfx.addCommand({
-  name: 'play_with_mpv',
-  description: 'Play the focused link with MPV'
-}, ({vim}) => {
-  vimfx.send(vim, 'getCurrentHref', null, url => {
-    let file = Cc['@mozilla.org/file/local;1'].createInstance(Ci.nsIFile)
-    file.initWithPath(mpv_path)
-
-    let process = Cc['@mozilla.org/process/util;1'].createInstance(Ci.nsIProcess)
-    process.init(file)
-
-    let args = mpv_options.split(' ')
-
-    if (url.includes('youtube.com')) {
-      // Parse url params to an object like:
-      // {"v":"g04s2u30NfQ","index":"3","list":"PL58H4uS5fMRzmMC_SfMelnCoHgB8COa5r"}
-      let qs = (function(a) {
-        if (a == '') return {}
-        let b = {}
-        for (let i = 0; i < a.length; ++i) {
-          let p = a[i].split('=', 2)
-          if (p.length == 1) {
-            b[p[0]] = ''
-          } else {
-            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '))
-          }
-        }
-        return b
-      })(url.substr(1).split('&'))
-
-      if (qs['list'] && qs['index']) {
-        // Example args: ['--video-unscaled=yes', '--ytdl-raw-options=format=best']
-        // So check for ytdl-raw-options.
-        let ytdlRawOptionsIndex = -1
-        for (let i = 0; i < args.length; i++) {
-          if (args[i].includes('ytdl-raw-options')) {
-            ytdlRawOptionsIndex = i
-            break
-          }
-        }
-
-        if (ytdlRawOptionsIndex > -1) {
-            args[ytdlRawOptionsIndex] += `,yes-playlist=,playlist-start=${qs['index']}`
-        } else {
-            args.push(`--ytdl-raw-options=yes-playlist=,playlist-start=${qs['index']}`)
-        }
-      }
-    }
-
-    args.push(url)
-
-    // process.run(false, args, args.length)
-    process.runAsync(args, args.length)
-  })
-})
-vimfx.set('custom.mode.normal.play_with_mpv', 'gm')
-//
-
+// Open downthemall
 vimfx.addCommand({
   name: 'downthemall_click_toolbar_button',
   description: 'DownThemAll',
 }, ({vim}) => {
   vim.window.document.getElementById('dta-button').click()
 })
-vimfx.set('custom.mode.normal.downthemall_click_toolbar_button', '#')
+vimfx.set('custom.mode.normal.downthemall_click_toolbar_button', '3')
+//
